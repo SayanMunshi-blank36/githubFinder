86d818c2cc5c098d4169614de92d18a05a34dcea
Sayan <sayanmunshi36@gmail.com>
2022-12-26 11:22:46 +0530

    Deployment

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        React JS Project to find Github Profiles using the github api.
+      </p>
+    </>
+  );
+};
+
+export default About;
